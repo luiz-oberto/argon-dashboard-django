02ca8297d5823ab19143e1e7cbbb12da72ec7002
Luiz Oberto <lom.raiol@gmail.com>
2025-03-21 11:08:59 -0300

    Formulario de registro e login de detentores funcionando

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -5,7 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from apps.home.models import Detentor
 
 
 class LoginForm(forms.Form):
@@ -56,5 +57,5 @@ class SignUpForm(UserCreationForm):
         ))
 
     class Meta:
-        model = User
+        model = Detentor
         fields = ('username', 'email', 'password1', 'password2')
